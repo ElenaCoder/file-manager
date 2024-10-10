1a22eafa6f7e0f2fd0e22cb99bbfe7d84dcf4fd0
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-10 10:54:34 +0300

    feat: add handleFileOperations as entry point for file operations

---
+++ commands/file/handleFileOperations.js
@@ -0,0 +1,31 @@
+import readFileContent from './fileOperations/readFileContent.js';
+import createFile from './fileOperations/createFile.js';
+import renameFile from './fileOperations/renameFile.js';
+import copyFile from './fileOperations/copyFile.js';
+import moveFile from './fileOperations/moveFile.js';
+import removeFile from './fileOperations/removeFile.js';
+
+function handleFileOperations(command, args, rl) {
+  switch (command) {
+    case 'cat':
+      readFileContent(args[0], rl);
+      break;
+    case 'add':
+      createFile(args[0], rl);
+      break;
+    case 'rn':
+      renameFile(args[0], args[1], rl);
+      break;
+    case 'cp':
+      copyFile(args[0], args[1], rl);
+      break;
+    case 'mv':
+      moveFile(args[0], args[1]);
+      break;
+    case 'rm':
+      removeFile(args[0], rl);
+      break;
+  }
+}
+
+export default handleFileOperations;
