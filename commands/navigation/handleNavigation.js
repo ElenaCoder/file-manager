d33c2f6462b2bc9eb11c9ee55e75d946bb41e164
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-10 10:44:48 +0300

    feat: add handleNavigation as entry point for wd navigation

---
+++ commands/navigation/handleNavigation.js
@@ -0,0 +1,19 @@
+import goUp from './navigationOperations/goUp.js';
+import changeDirectory from './navigationOperations/changeDirectory.js';
+import listDirectory from './navigationOperations/listDirectory.js'
+
+function handleNavigation(command, args, rl) {
+  switch (command) {
+    case 'up':
+      goUp();
+      break;
+    case 'cd':
+      changeDirectory(args[0]);
+      break;
+    case 'ls':
+      listDirectory(rl);
+      break;
+  }
+}
+
+export default handleNavigation;
