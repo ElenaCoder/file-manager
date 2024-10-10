760a22793cfada3b1ca7fdc06668f2b2f3e6082a
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-10 12:18:24 +0300

    feat: implement handleHashCalculator function

---
+++ commands/hash/handleHashCalculation.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+
+async function handleHashCalculation(filePath, rl) {
+  try {
+    const absolutePath = path.resolve(process.cwd(), filePath);
+    
+    if (!fs.existsSync(absolutePath)) {
+      console.log('Operation failed: File does not exist.');
+      return;
+    }
+
+    const readableStream = fs.createReadStream(absolutePath);
+    
+    const hash = crypto.createHash('sha256');
+    
+    readableStream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+    
+    readableStream.on('end', () => {
+      const hashResult = hash.digest('hex');
+      console.log(`Hash for file ${filePath}: ${hashResult}`);
+    });
+    
+    readableStream.on('error', (err) => {
+      console.log('Operation failed: Error reading the file.');
+    });
+  } catch (err) {
+    console.log('Operation failed: An unexpected error occurred.');
+  } finally {
+    rl.prompt();
+  }
+}
+
+export default handleHashCalculation;
