440899d87c4021062e62320ceb48c8affaa7d2c6
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-10 10:40:08 +0300

    feat: add handleUserInput as entry point for all inputs

---
+++ input/handleUserInput.js
@@ -0,0 +1,36 @@
+import handleNavigation  from '../commands/navigation/handleNavigation.js';
+import handleFileOperations from '../commands/file/handleFileOperations.js';
+import handleOSCommands from '../commands/os/handleOsCommands.js';
+import utils from '../utils/utils.js';
+
+function handleUserInput(input, rl) {
+  const [command, ...args] = input.trim().split(' ');
+
+  switch (command) {
+    case 'up':
+    case 'cd':
+    case 'ls':
+      handleNavigation(command, args, rl);
+      break;
+    case 'cat':
+    case 'add':
+    case 'rn':
+    case 'cp':
+    case 'mv':
+    case 'rm':
+      handleFileOperations(command, args, rl);
+      break;
+    case 'os':
+      handleOSCommands(args[0]);
+      break;
+    case '.exit':
+      rl.close();
+      break;
+    default:
+      console.log('Invalid input');
+  }
+
+  utils.printCurrentDirectory();
+}
+
+export default { handleUserInput };
