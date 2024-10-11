1b8b7607b63b028467005467bbc26a1bb8bbc6f6
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-11 12:15:04 +0300

    feat: update compressFile function

--- commands/compression/compressionOperations/compressFile.js
+++ commands/compression/compressionOperations/compressFile.js
@@ -7,27 +7,32 @@ async function compressFile(src, dest, rl) {
     const absoluteSrcPath = path.resolve(process.cwd(), src);
     const absoluteDestPath = path.resolve(process.cwd(), dest, path.basename(src)) + '.br';
 
+    if (!fs.existsSync(absoluteSrcPath)) {
+      console.log(`Source file does not exist: ${absoluteSrcPath}`);
+      return;
+    }
+
+    console.log(`\nCompressing...`);
+
     const readableStream = fs.createReadStream(absoluteSrcPath);
     const writableStream = fs.createWriteStream(absoluteDestPath);
     const brotliCompress = zlib.createBrotliCompress();
 
-    readableStream.pipe(brotliCompress).pipe(writableStream);
-
-    writableStream.on('finish', () => {
-      console.log(`\n${src} compressed to ${absoluteDestPath}`);
-    });
-
-    readableStream.on('error', () => {
-      console.log('Operation failed');
-    });
-
-    writableStream.on('error', () => {
-      console.log('Operation failed');
+    return new Promise((resolve, reject) => {
+      readableStream
+        .pipe(brotliCompress)
+        .pipe(writableStream)
+        .on('finish', () => {
+          console.log(`\n${src} successfully compressed to ${absoluteDestPath}`);
+          resolve();
+        })
+        .on('error', (err) => {
+          console.log('Compression failed due to an error:', err.message);
+          reject(err);
+        });
     });
   } catch (err) {
-    console.log('Operation failed');
-  } finally {
-    rl.prompt();
+    console.log('Operation failed:', err.message);
   }
 }
 
